cf1ca4ed877727a490306a1d4389bdfde35794c5
Marco von Baumbach <marco.vonbaumbach@gmx.de>
2022-12-06 09:32:12 +0100

    base-stats

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 let currentPokemon;
+let currentPokemonBreeding;
+let totalStat;
 
 async function loadPokemon() {
     let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
-    let url2 = 'https://pokeapi.co/api/v2/pokemon/bulbasaur';
+    let url2 = 'https://pokeapi.co/api/v2/pokemon-species/charmander';
     let response = await fetch(url);
     currentPokemon = await response.json();
     console.log(currentPokemon);
+    let response2 = await fetch(url2);
+    currentPokemonBreeding = await response2.json();
+    console.log(currentPokemonBreeding);
     renderPokemonInfo();
     renderAbout();
 }
 
 function renderPokemonInfo() {
-    document.getElementById('pokemon-name').innerHTML = currentPokemon.name; 
+    document.getElementById('pokemon-name').innerHTML = currentPokemonBreeding.names[5].name; 
     document.getElementById('pokemon-image').src = currentPokemon.sprites.other.dream_world.front_default;
     renderPokemonType();
     renderPokemonId();
@@ -41,7 +46,7 @@ function renderAbout() {
     <table>
         <tr>
             <td>Species</td>
-            <td>${currentPokemon.types[0].type.name}</td>
+            <td>${currentPokemonBreeding.genera[7].genus}</td>
         </tr>
         <tr>
             <td>Height</td>
@@ -59,20 +64,21 @@ function renderAbout() {
     <h3>Breeding</h3>
     <table>
         <tr>
-            <td>Species</td>
-            <td>${currentPokemon.types[0].type.name}</td>
+            <td>Gender</td>
+            <td><img class="gender" src="./img/male-16.png"> ${currentPokemonBreeding.gender_rate * 100}%    <img class="gender" src="./img/female-16.png"> ${(1 - currentPokemonBreeding.gender_rate) * 100}%</td>
         </tr>
         <tr>
-            <td>Height</td>
-            <td>${((currentPokemon.height * 10) / 2.54).toFixed(2)}" (${currentPokemon.height * 10} cm)</td>
+            <td>Egg Groups</td>
+            <td id="egg-groups"></td>
         </tr>
         <tr>
-            <td>Weight</td>
-            <td>${currentPokemon.weight / 10} lbs (${(currentPokemon.weight / 22.05).toFixed(2)} kg)</td>
+            <td>Egg Cycles</td>
+            <td>${currentPokemonBreeding.hatch_counter}</td>
         </tr>
     </table>
     `;
     renderAbilities();
+    renderEggGroups();
 }
 
 function renderAbilities() {
@@ -80,4 +86,92 @@ function renderAbilities() {
         document.getElementById('abilities').innerHTML += `${currentPokemon.abilities[i].ability.name}, `;
     }
     document.getElementById('abilities').innerHTML += currentPokemon.abilities[currentPokemon.abilities.length - 1].ability.name;
+}
+
+function renderEggGroups() {
+    for (let i = 0; i < currentPokemonBreeding.egg_groups.length - 1; i++) {
+        document.getElementById('egg-groups').innerHTML += `${currentPokemonBreeding.egg_groups[i].name}, `;
+    }
+    document.getElementById('egg-groups').innerHTML += currentPokemonBreeding.egg_groups[currentPokemonBreeding.egg_groups.length - 1].name;
+}
+
+function renderBaseStats() {
+    document.getElementById('info-container-content').innerHTML = `
+    <table>
+        <tr>
+            <td>HP</td>
+            <td class="d-flex">${currentPokemon.stats[0].base_stat}<div class="base-stats-gap" id="base-stat-0"></div><div id="base-stat-gray-0"></div></td>
+        </tr>
+        <tr>
+            <td>Attack</td>
+            <td class="d-flex">${currentPokemon.stats[1].base_stat}<div class="base-stats-gap" id="base-stat-1"></div><div id="base-stat-gray-1"></div></td>
+        </tr>
+        <tr>
+            <td>Defense</td>
+            <td class="d-flex">${currentPokemon.stats[2].base_stat}<div class="base-stats-gap" id="base-stat-2"></div><div id="base-stat-gray-2"></div></td>
+        </tr>
+        <tr>
+            <td>Sp. Atk</td>
+            <td class="d-flex">${currentPokemon.stats[3].base_stat}<div class="base-stats-gap" id="base-stat-3"></div><div id="base-stat-gray-3"></div></td>
+        </tr>
+        <tr>
+            <td>Sp. Def</td>
+            <td class="d-flex">${currentPokemon.stats[4].base_stat}<div class="base-stats-gap" id="base-stat-4"></div><div id="base-stat-gray-4"></div></td>
+        </tr>
+        <tr>
+            <td>Speed</td>
+            <td class="d-flex">${currentPokemon.stats[5].base_stat}<div class="base-stats-gap" id="base-stat-5"></div><div id="base-stat-gray-5"></div></td>
+        </tr>
+        <tr>
+            <td>Total</td>
+            <td class="d-flex"><div id="total-base-stats"></div><div class="base-stats-gap" id="base-stat-6"></div><div id="base-stat-gray-6"></div></td>
+        </tr>
+    </table>
+    <h3>Type Defenses</h3>
+    <span class="font-color-gray">The effectiveness of each type on ${currentPokemonBreeding.names[5].name}</span>
+    `;
+    totalBaseStats();
+    baseStatsBar();
+    totalBaseStatsBar();
+}
+
+function totalBaseStats () {
+    document.getElementById('total-base-stats').innerHTML =`
+            ${  totalStat =
+                currentPokemon.stats[0].base_stat
+                + currentPokemon.stats[1].base_stat
+                + currentPokemon.stats[2].base_stat
+                + currentPokemon.stats[3].base_stat
+                + currentPokemon.stats[4].base_stat
+                + currentPokemon.stats[5].base_stat
+            }     
+    `;
+}
+
+function baseStatsBar() {
+    for (let i = 0; i < 6; i++) {
+        document.getElementById(`base-stat-${i}`).style.height = '5px';
+        document.getElementById(`base-stat-${i}`).style.width = currentPokemon.stats[i].base_stat + 'px';
+        if(currentPokemon.stats[i].base_stat > 49) {
+            document.getElementById(`base-stat-${i}`).style.backgroundColor = 'green';
+        } else {
+            document.getElementById(`base-stat-${i}`).style.backgroundColor = 'red';
+        }
+        document.getElementById(`base-stat-gray-${i}`).style.width = (100 - currentPokemon.stats[i].base_stat) + 'px';
+        document.getElementById(`base-stat-gray-${i}`).style.height = '5px';
+        document.getElementById(`base-stat-gray-${i}`).style.backgroundColor = 'lightgray';
+    }
+}
+
+function totalBaseStatsBar() {
+    document.getElementById(`base-stat-6`).style.height = '5px';
+    document.getElementById(`base-stat-6`).style.width = (totalStat / 6) + 'px';
+    if(totalStat > 299) {
+        document.getElementById(`base-stat-6`).style.backgroundColor = 'green';
+    } else {
+        document.getElementById(`base-stat-6`).style.backgroundColor = 'red';
+    }
+    document.getElementById(`base-stat-gray-6`).style.width = (100 - (totalStat / 6)) + 'px';
+    document.getElementById(`base-stat-gray-6`).style.height = '5px';
+    document.getElementById(`base-stat-gray-6`).style.backgroundColor = 'lightgray';
 }
